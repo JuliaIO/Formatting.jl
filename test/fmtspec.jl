eda88ff6c91a9fac57681519b8789a6b515999e5
ScottPJones <scottjones@alum.mit.edu>
2017-01-25 20:46:31 -0500

    Update tests to use pyfmt instead of fmt, use testsets

--- test/fmtspec.jl
+++ test/fmtspec.jl
@@ -5,175 +5,181 @@ using Base.Test
 
 
 # default spec
-fs = FormatSpec("")
-@test fs.typ == 's'
-@test fs.fill == ' '
-@test fs.align == '<'
-@test fs.sign == '-'
-@test fs.width == -1
-@test fs.prec == -1
-@test fs.ipre == false
-@test fs.zpad == false
-@test fs.tsep == false
+@testset "Default spec" begin
+    fs = FormatSpec("")
+    @test fs.typ == 's'
+    @test fs.fill == ' '
+    @test fs.align == '<'
+    @test fs.sign == '-'
+    @test fs.width == -1
+    @test fs.prec == -1
+    @test fs.ipre == false
+    @test fs.zpad == false
+    @test fs.tsep == false
+end
 
 # more cases
 
-fs = FormatSpec("d")
-@test fs == FormatSpec('d')
-@test fs.align == '>'
+@testset "FormatSpec(\"d\")" begin
+    fs = FormatSpec("d")
+    @test fs == FormatSpec('d')
+    @test fs.align == '>'
 
-@test FormatSpec("8x") == FormatSpec('x'; width=8)
-@test FormatSpec("08b") == FormatSpec('b'; width=8, zpad=true)
-@test FormatSpec("12f") == FormatSpec('f'; width=12, prec=6)
-@test FormatSpec("12.7f") == FormatSpec('f'; width=12, prec=7)
-@test FormatSpec("+08o") == FormatSpec('o'; width=8, zpad=true, sign='+')
+    @test FormatSpec("8x") == FormatSpec('x'; width=8)
+    @test FormatSpec("08b") == FormatSpec('b'; width=8, zpad=true)
+    @test FormatSpec("12f") == FormatSpec('f'; width=12, prec=6)
+    @test FormatSpec("12.7f") == FormatSpec('f'; width=12, prec=7)
+    @test FormatSpec("+08o") == FormatSpec('o'; width=8, zpad=true, sign='+')
 
-@test FormatSpec("8") == FormatSpec('s'; width=8)
-@test FormatSpec(".6f") == FormatSpec('f'; prec=6)
-@test FormatSpec("<8d") == FormatSpec('d'; width=8, align='<')
-@test FormatSpec("#<8d") == FormatSpec('d'; width=8, fill='#', align='<')
-@test FormatSpec("#8,d") == FormatSpec('d'; width=8, ipre=true, tsep=true)
+    @test FormatSpec("8") == FormatSpec('s'; width=8)
+    @test FormatSpec(".6f") == FormatSpec('f'; prec=6)
+    @test FormatSpec("<8d") == FormatSpec('d'; width=8, align='<')
+    @test FormatSpec("#<8d") == FormatSpec('d'; width=8, fill='#', align='<')
+    @test FormatSpec("#8,d") == FormatSpec('d'; width=8, ipre=true, tsep=true)
+end
 
-# format string
+@testset "Format string" begin
+    @test pyfmt("", "abc") == "abc"
+    @test pyfmt("s", "abc") == "abc"
+    @test pyfmt("2s", "abc") == "abc"
+    @test pyfmt("5s", "abc") == "abc  "
+    @test pyfmt(">5s", "abc") == "  abc"
+    @test pyfmt("*>5s", "abc") == "**abc"
+    @test pyfmt("*<5s", "abc") == "abc**"
+end
 
-@test fmt("", "abc") == "abc"
-@test fmt("s", "abc") == "abc"
-@test fmt("2s", "abc") == "abc"
-@test fmt("5s", "abc") == "abc  "
-@test fmt(">5s", "abc") == "  abc"
-@test fmt("*>5s", "abc") == "**abc"
-@test fmt("*<5s", "abc") == "abc**"
+@testset "Format Char" begin
+    @test pyfmt("", 'c') == "c"
+    @test pyfmt("c", 'c') == "c"
+    @test pyfmt("3c", 'c') == "c  "
+    @test pyfmt(">3c", 'c') == "  c"
+    @test pyfmt("*>3c", 'c') == "**c"
+    @test pyfmt("*<3c", 'c') == "c**"
+end
 
-# format char
+@testset "Format integer" begin
 
-@test fmt("", 'c') == "c"
-@test fmt("c", 'c') == "c"
-@test fmt("3c", 'c') == "c  "
-@test fmt(">3c", 'c') == "  c"
-@test fmt("*>3c", 'c') == "**c"
-@test fmt("*<3c", 'c') == "c**"
+    @test pyfmt("", 1234) == "1234"
+    @test pyfmt("d", 1234) == "1234"
+    @test pyfmt("n", 1234) == "1234"
+    @test pyfmt("x", 0x2ab) == "2ab"
+    @test pyfmt("X", 0x2ab) == "2AB"
+    @test pyfmt("o", 0o123) == "123"
+    @test pyfmt("b", 0b1101) == "1101"
 
-# format integer
+    @test pyfmt("d", 0) == "0"
+    @test pyfmt("d", 9) == "9"
+    @test pyfmt("d", 10) == "10"
+    @test pyfmt("d", 99) == "99"
+    @test pyfmt("d", 100) == "100"
+    @test pyfmt("d", 1000) == "1000"
 
-@test fmt("", 1234) == "1234"
-@test fmt("d", 1234) == "1234"
-@test fmt("n", 1234) == "1234"
-@test fmt("x", 0x2ab) == "2ab"
-@test fmt("X", 0x2ab) == "2AB"
-@test fmt("o", 0o123) == "123"
-@test fmt("b", 0b1101) == "1101"
+    @test pyfmt("06d", 123) == "000123"
+    @test pyfmt("+6d", 123) == "  +123"
+    @test pyfmt("+06d", 123) == "+00123"
+    @test pyfmt(" d", 123) == " 123"
+    @test pyfmt(" 6d", 123) == "   123"
+    @test pyfmt("<6d", 123) == "123   "
+    @test pyfmt(">6d", 123) == "   123"
+    @test pyfmt("*<6d", 123) == "123***"
+    @test pyfmt("*>6d", 123) == "***123"
+    @test pyfmt("< 6d", 123) == " 123  "
+    @test pyfmt("<+6d", 123) == "+123  "
+    @test pyfmt("> 6d", 123) == "   123"
+    @test pyfmt(">+6d", 123) == "  +123"
 
-@test fmt("d", 0) == "0"
-@test fmt("d", 9) == "9"
-@test fmt("d", 10) == "10"
-@test fmt("d", 99) == "99"
-@test fmt("d", 100) == "100"
-@test fmt("d", 1000) == "1000"
+    @test pyfmt("+d", -123) == "-123"
+    @test pyfmt("-d", -123) == "-123"
+    @test pyfmt(" d", -123) == "-123"
+    @test pyfmt("06d", -123) == "-00123"
+    @test pyfmt("<6d", -123) == "-123  "
+    @test pyfmt(">6d", -123) == "  -123"
 
-@test fmt("06d", 123) == "000123"
-@test fmt("+6d", 123) == "  +123"
-@test fmt("+06d", 123) == "+00123"
-@test fmt(" d", 123) == " 123"
-@test fmt(" 6d", 123) == "   123"
-@test fmt("<6d", 123) == "123   "
-@test fmt(">6d", 123) == "   123"
-@test fmt("*<6d", 123) == "123***"
-@test fmt("*>6d", 123) == "***123"
-@test fmt("< 6d", 123) == " 123  "
-@test fmt("<+6d", 123) == "+123  "
-@test fmt("> 6d", 123) == "   123"
-@test fmt(">+6d", 123) == "  +123"
+@testset "Format floating point (f)" begin
 
-@test fmt("+d", -123) == "-123"
-@test fmt("-d", -123) == "-123"
-@test fmt(" d", -123) == "-123"
-@test fmt("06d", -123) == "-00123"
-@test fmt("<6d", -123) == "-123  "
-@test fmt(">6d", -123) == "  -123"
+    @test pyfmt("", 0.125) == "0.125"
+    @test pyfmt("f", 0.0) == "0.000000"
+    @test pyfmt("f", 0.001) == "0.001000"
+    @test pyfmt("f", 0.125) == "0.125000"
+    @test pyfmt("f", 1.0/3) == "0.333333"
+    @test pyfmt("f", 1.0/6) == "0.166667"
+    @test pyfmt("f", -0.125) == "-0.125000"
+    @test pyfmt("f", -1.0/3) == "-0.333333"
+    @test pyfmt("f", -1.0/6) == "-0.166667"
+    @test pyfmt("f", 1234.5678) == "1234.567800"
+    @test pyfmt("8f", 1234.5678) == "1234.567800"
 
-# format floating point (f)
+    @test pyfmt("8.2f", 8.376) == "    8.38"
+    @test pyfmt("<8.2f", 8.376) == "8.38    "
+    @test pyfmt(">8.2f", 8.376) == "    8.38"
+    @test pyfmt("8.2f", -8.376) == "   -8.38"
+    @test pyfmt("<8.2f", -8.376) == "-8.38   "
+    @test pyfmt(">8.2f", -8.376) == "   -8.38"
 
-@test fmt("", 0.125) == "0.125"
-@test fmt("f", 0.0) == "0.000000"
-@test fmt("f", 0.001) == "0.001000"
-@test fmt("f", 0.125) == "0.125000"
-@test fmt("f", 1.0/3) == "0.333333"
-@test fmt("f", 1.0/6) == "0.166667"
-@test fmt("f", -0.125) == "-0.125000"
-@test fmt("f", -1.0/3) == "-0.333333"
-@test fmt("f", -1.0/6) == "-0.166667"
-@test fmt("f", 1234.5678) == "1234.567800"
-@test fmt("8f", 1234.5678) == "1234.567800"
+    @test pyfmt("<08.2f", 8.376) == "00008.38"
+    @test pyfmt(">08.2f", 8.376) == "00008.38"
+    @test pyfmt("<08.2f", -8.376) == "-0008.38"
+    @test pyfmt(">08.2f", -8.376) == "-0008.38"
+    @test pyfmt("*<8.2f", 8.376) == "8.38****"
+    @test pyfmt("*>8.2f", 8.376) == "****8.38"
+    @test pyfmt("*<8.2f", -8.376) == "-8.38***"
+    @test pyfmt("*>8.2f", -8.376) == "***-8.38"
 
-@test fmt("8.2f", 8.376) == "    8.38"
-@test fmt("<8.2f", 8.376) == "8.38    "
-@test fmt(">8.2f", 8.376) == "    8.38"
-@test fmt("8.2f", -8.376) == "   -8.38"
-@test fmt("<8.2f", -8.376) == "-8.38   "
-@test fmt(">8.2f", -8.376) == "   -8.38"
+    @test pyfmt(".2f", 0.999) == "1.00"
+    @test pyfmt(".2f", 0.996) == "1.00"
+    # Floating point error can upset this one (i.e. 0.99500000 or 0.994999999)
+    @test (pyfmt(".2f", 0.995) == "1.00" || pyfmt(".2f", 0.995) == "0.99")
+    @test pyfmt(".2f", 0.994) == "0.99"
+end
 
-@test fmt("<08.2f", 8.376) == "00008.38"
-@test fmt(">08.2f", 8.376) == "00008.38"
-@test fmt("<08.2f", -8.376) == "-0008.38"
-@test fmt(">08.2f", -8.376) == "-0008.38"
-@test fmt("*<8.2f", 8.376) == "8.38****"
-@test fmt("*>8.2f", 8.376) == "****8.38"
-@test fmt("*<8.2f", -8.376) == "-8.38***"
-@test fmt("*>8.2f", -8.376) == "***-8.38"
+@testset "Format floating point (e)" begin
 
-@test fmt(".2f", 0.999) == "1.00"
-@test fmt(".2f", 0.996) == "1.00"
-# Floating point error can upset this one (i.e. 0.99500000 or 0.994999999)
-@test (fmt(".2f", 0.995) == "1.00" || fmt(".2f", 0.995) == "0.99")
-@test fmt(".2f", 0.994) == "0.99"
+    @test pyfmt("E", 0.0) == "0.000000E+00"
+    @test pyfmt("e", 0.0) == "0.000000e+00"
+    @test pyfmt("e", 0.001) == "1.000000e-03"
+    @test pyfmt("e", 0.125) == "1.250000e-01"
+    @test pyfmt("e", 100/3) == "3.333333e+01"
+    @test pyfmt("e", -0.125) == "-1.250000e-01"
+    @test pyfmt("e", -100/6) == "-1.666667e+01"
+    @test pyfmt("e", 1234.5678) == "1.234568e+03"
+    @test pyfmt("8e", 1234.5678) == "1.234568e+03"
 
-# format floating point (e)
+    @test pyfmt("<12.2e", 13.89) == "1.39e+01    "
+    @test pyfmt(">12.2e", 13.89) == "    1.39e+01"
+    @test pyfmt("*<12.2e", 13.89) == "1.39e+01****"
+    @test pyfmt("*>12.2e", 13.89) == "****1.39e+01"
+    @test pyfmt("012.2e", 13.89) == "00001.39e+01"
+    @test pyfmt("012.2e", -13.89) == "-0001.39e+01"
+    @test pyfmt("+012.2e", 13.89) == "+0001.39e+01"
 
-@test fmt("E", 0.0) == "0.000000E+00"
-@test fmt("e", 0.0) == "0.000000e+00"
-@test fmt("e", 0.001) == "1.000000e-03"
-@test fmt("e", 0.125) == "1.250000e-01"
-@test fmt("e", 100/3) == "3.333333e+01"
-@test fmt("e", -0.125) == "-1.250000e-01"
-@test fmt("e", -100/6) == "-1.666667e+01"
-@test fmt("e", 1234.5678) == "1.234568e+03"
-@test fmt("8e", 1234.5678) == "1.234568e+03"
+    @test pyfmt(".1e", 0.999) == "1.0e+00"
+    @test pyfmt(".1e", 0.996) == "1.0e+00"
+    # Floating point error can upset this one (i.e. 0.99500000 or 0.994999999)
+    @test (pyfmt(".1e", 0.995) == "1.0e+00" || pyfmt(".1e", 0.995) == "9.9e-01")
+    @test pyfmt(".1e", 0.994) == "9.9e-01"
+    @test pyfmt(".1e", 0.6) == "6.0e-01"
+    @test pyfmt(".1e", 0.9) == "9.0e-01"
+end
 
-@test fmt("<12.2e", 13.89) == "1.39e+01    "
-@test fmt(">12.2e", 13.89) == "    1.39e+01"
-@test fmt("*<12.2e", 13.89) == "1.39e+01****"
-@test fmt("*>12.2e", 13.89) == "****1.39e+01"
-@test fmt("012.2e", 13.89) == "00001.39e+01"
-@test fmt("012.2e", -13.89) == "-0001.39e+01"
-@test fmt("+012.2e", 13.89) == "+0001.39e+01"
+@testset "Format special floating point value" begin
 
-@test fmt(".1e", 0.999) == "1.0e+00"
-@test fmt(".1e", 0.996) == "1.0e+00"
-# Floating point error can upset this one (i.e. 0.99500000 or 0.994999999)
-@test (fmt(".1e", 0.995) == "1.0e+00" || fmt(".1e", 0.995) == "9.9e-01")
-@test fmt(".1e", 0.994) == "9.9e-01"
-@test fmt(".1e", 0.6) == "6.0e-01"
-@test fmt(".1e", 0.9) == "9.0e-01"
+    @test pyfmt("f", NaN) == "NaN"
+    @test pyfmt("e", NaN) == "NaN"
+    @test pyfmt("f", NaN32) == "NaN"
+    @test pyfmt("e", NaN32) == "NaN"
 
-# format special floating point value
+    @test pyfmt("f", Inf) == "Inf"
+    @test pyfmt("e", Inf) == "Inf"
+    @test pyfmt("f", Inf32) == "Inf"
+    @test pyfmt("e", Inf32) == "Inf"
 
-@test fmt("f", NaN) == "NaN"
-@test fmt("e", NaN) == "NaN"
-@test fmt("f", NaN32) == "NaN"
-@test fmt("e", NaN32) == "NaN"
+    @test pyfmt("f", -Inf) == "-Inf"
+    @test pyfmt("e", -Inf) == "-Inf"
+    @test pyfmt("f", -Inf32) == "-Inf"
+    @test pyfmt("e", -Inf32) == "-Inf"
 
-@test fmt("f", Inf) == "Inf"
-@test fmt("e", Inf) == "Inf"
-@test fmt("f", Inf32) == "Inf"
-@test fmt("e", Inf32) == "Inf"
-
-@test fmt("f", -Inf) == "-Inf"
-@test fmt("e", -Inf) == "-Inf"
-@test fmt("f", -Inf32) == "-Inf"
-@test fmt("e", -Inf32) == "-Inf"
-
-@test fmt("<5f", Inf) == "Inf  "
-@test fmt(">5f", Inf) == "  Inf"
-@test fmt("*<5f", Inf) == "Inf**"
-@test fmt("*>5f", Inf) == "**Inf"
-
+    @test pyfmt("<5f", Inf) == "Inf  "
+    @test pyfmt(">5f", Inf) == "  Inf"
+    @test pyfmt("*<5f", Inf) == "Inf**"
+    @test pyfmt("*>5f", Inf) == "**Inf"
+end
--- test/formatexpr.jl
+++ test/formatexpr.jl
@@ -1,36 +1,39 @@
 using Format
 using Base.Test
 
-# with positional arguments
+@testset "format with positional arguments" begin
 
-@test format("{1}", 10) == "10"
-@test format("abc {1}", 10) == "abc 10"
-@test format("{1} efg", 10) == "10 efg"
-@test format("abc {1} efg", 10) == "abc 10 efg"
-@test format("{1} + {2}", 10, "xyz") == "10 + xyz"
-@test format("abc {1} + {2}", 10, "xyz") == "abc 10 + xyz"
-@test format("{1} + {2} efg", 10, "xyz") == "10 + xyz efg"
-@test format("abc {1} + {2} efg", 10, "xyz") == "abc 10 + xyz efg"
-@test format("abc {1}{2} efg", 10, "xyz") == "abc 10xyz efg"
+    @test format("{1}", 10) == "10"
+    @test format("abc {1}", 10) == "abc 10"
+    @test format("{1} efg", 10) == "10 efg"
+    @test format("abc {1} efg", 10) == "abc 10 efg"
+    @test format("{1} + {2}", 10, "xyz") == "10 + xyz"
+    @test format("abc {1} + {2}", 10, "xyz") == "abc 10 + xyz"
+    @test format("{1} + {2} efg", 10, "xyz") == "10 + xyz efg"
+    @test format("abc {1} + {2} efg", 10, "xyz") == "abc 10 + xyz efg"
+    @test format("abc {1}{2} efg", 10, "xyz") == "abc 10xyz efg"
 
-@test format("{1:d} + {2:s}", 10, "xyz") == "10 + xyz"
-@test format("{1:04d} + {2:*>5}", 10, "xyz") == "0010 + **xyz"
-@test format("let {2:<5} := {1:.4f};", 12.3, "var") == "let var   := 12.3000;"
+    @test format("{1:d} + {2:s}", 10, "xyz") == "10 + xyz"
+    @test format("{1:04d} + {2:*>5}", 10, "xyz") == "0010 + **xyz"
+    @test format("let {2:<5} := {1:.4f};", 12.3, "var") == "let var   := 12.3000;"
 
-@test format("{}", 10) == "10"
-@test format("{} + {}", 10, 20) == "10 + 20"
-@test format("{} + {:04d}", 10, 20) == "10 + 0020"
-@test format("{:03d} + {}", 10, 20) == "010 + 20"
-@test format("{:03d} + {:04d}", 10, 20) == "010 + 0020"
-@test_throws(ErrorException, format("{1} + {}", 10, 20) )
-@test_throws(ErrorException, format("{} + {1}", 10, 20) )
+    @test format("{}", 10) == "10"
+    @test format("{} + {}", 10, 20) == "10 + 20"
+    @test format("{} + {:04d}", 10, 20) == "10 + 0020"
+    @test format("{:03d} + {}", 10, 20) == "010 + 20"
+    @test format("{:03d} + {:04d}", 10, 20) == "010 + 0020"
+    @test_throws(ErrorException, format("{1} + {}", 10, 20) )
+    @test_throws(ErrorException, format("{} + {1}", 10, 20) )
+end
 
-# escape {{ and }}
+@testset "format escape {{ and }}" begin
 
-@test format("{{}}") == "{}"
-@test format("{{{1}}}", 10) == "{10}"
-@test format("v: {{{2}}} = {1:.4f}", 1.2, "ab") == "v: {ab} = 1.2000"
+    @test format("{{}}") == "{}"
+    @test format("{{{1}}}", 10) == "{10}"
+    @test format("v: {{{2}}} = {1:.4f}", 1.2, "ab") == "v: {ab} = 1.2000"
+end
 
-# with filter
-@test format("{1|>abs2} + {2|>abs2:.2f}", 2, 3) == "4 + 9.00"
+@testset "format with filter" begin
+    @test format("{1|>abs2} + {2|>abs2:.2f}", 2, 3) == "4 + 9.00"
+end
 
