f1ee9b37532adcccbb8aa0249948d05b31a4d9ef
Sukera <Seelengrab@users.noreply.github.com>
2024-02-24 14:55:34 +0100

    Add deprecation notice on `using`
    This adds a deprecation notice that will be printed on `using Formatting`,
    to inform users that the package has been deprecated and that they should
    consider using a different package.

--- src/Formatting.jl
+++ src/Formatting.jl
@@ -1,12 +1,23 @@
 module Formatting
 
     import Base.show
-    using Printf
+    using Printf, Logging
 
     export
         FormatSpec, FormatExpr,
         printfmt, printfmtln, fmt, format,
         sprintf1, generate_formatter
+
+    function __init__()
+        @warn """
+        DEPRECATION NOTICE
+
+        This package has been unmaintained for a while, with some serious
+        bugs comprimising the original purpose of the package. As a result,
+        it has been deprecated - consider using an alternative, such as
+        `Format.jl` (https://github.com/JuliaString/Format.jl) or the `Printf` stdlib directly.
+        """
+    end
 
     include("cformat.jl" )
     include("fmtspec.jl")
